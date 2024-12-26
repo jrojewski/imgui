fcc43cea7cc83194fe45d99ad7bc504d56e582b4
jrojewski <57857936+jrojewski@users.noreply.github.com>
2024-12-26 12:11:28 +0100

    premake updated

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,14 @@
 project "ImGui"
-    kind "StaticLib"
-    language "C++"
-    
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
 
-    files
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    defines { "IMGUI_API=__declspec(dllexport)" }
+
+	files
 	{
 		"imconfig.h",
 		"imgui.h",
@@ -19,11 +22,20 @@ project "ImGui"
 		"imstb_truetype.h",
 		"imgui_demo.cpp"
 	}
-    
-    filter "system:windows"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
-        
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
